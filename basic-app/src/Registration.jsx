b6f91ca98c38fe7e12d166f16439ead89d7dc14d
Kazi Ahosun Habib Ripon <Habibkazi92@gmail.com>
2023-09-30 19:04:44 +0600

    form data send and reset

--- basic-app/src/Registration.jsx
+++ basic-app/src/Registration.jsx
@@ -1,24 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 import "./index.css";
 const Registration = () => {
-  const getInputs = event => {
-    event.preventDefault();
+  const [objData, setObjData] = useState({});
+  const getInputs = (value, name) => {
+    let data = { [name]: value };
+    setObjData({ ...objData, ...data });
+  };
+  const handleSubmit = e => {
+    e.preventDefault();
+    console.log(objData);
   };
   return (
-    <div className="form-wrapper">
-      <div className="form">
-        <h1>Registration Form</h1>
-        <input
-          type="text"
-          name="username"
-          placeholder="Username"
-          onChange={getInputs}
-        />
-        <input type="password" name="password" placeholder="Password" />
-        <input type="email" name="email" placeholder="Email" />
-        <input type="submit" value="Submit" className="btn" />
+    <form onSubmit={handleSubmit}>
+      <div className="form-wrapper">
+        <div className="form">
+          <h1>Registration Form</h1>
+          <input
+            type="text"
+            name="username"
+            placeholder="Username"
+            onChange={event => getInputs(event.target.value, event.target.name)}
+          />
+          <input
+            name="password"
+            onChange={event => getInputs(event.target.value, event.target.name)}
+            type="password"
+            placeholder="Password"
+          />
+          <input
+            onChange={event => getInputs(event.target.value, event.target.name)}
+            type="email"
+            name="email"
+            placeholder="Email"
+          />
+          <input
+            onChange={event => getInputs(event.target.value, event.target.name)}
+            type="date"
+            name="date"
+            placeholder="date"
+          />
+          <input
+            onChange={event => getInputs(event.target.value, event.target.name)}
+            type="file"
+            name="file"
+            placeholder="File"
+          />
+          <input type="submit" className="btn" />
+          <input type="reset" className="btn" />
+        </div>
       </div>
-    </div>
+    </form>
   );
 };
 
